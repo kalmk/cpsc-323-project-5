601dba8c834e3e0a1fd5e4b7f3a941bf4ae6b836
Lcui97 <123529233+Lcui97@users.noreply.github.com>
2025-05-08 05:33:30 +0000

    Made some changes to main.py & README

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from code_generator import CodeGenerator
 
 def main():
+    input_path = "test_cases/testCase1.txt"
+    output_path = "output.asm"
     # Read TAC instructions from external file 'testCase1.txt'
-    with open('testCase1.txt') as f:
+    with open(input_path) as f:
         tac = [line.strip() for line in f if line.strip()]
 
     cg = CodeGenerator()
     asm = cg.generate(tac)
 
     # Write the generated MIPS to 'output.asm'
-    with open('output.asm', 'w') as out:
+    with open(output_path, 'w') as out:
         out.write(asm)
 
     print("MIPS assembly written to output.asm")
